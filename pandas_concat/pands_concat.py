11d197a5f232f5b99820328fc0d64fbfbf0948cd
gnkm <genki.matsunaga@gmail.com>
2023-04-20 09:38:27 +0900

    Change var names

--- pandas_concat/pands_concat.py
+++ pandas_concat/pands_concat.py
@@ -1,22 +1,22 @@
 """https://blog.gnkm.info/pandas-concat の記事で扱ったソースコード
 """
-
+import sys
 import pandas as pd
 
 SEPARATOR = " ===== ===== ===== "
 
 
-def print_heading(title):
+def print_heading(title=''):
     print(f"\n{SEPARATOR} {title} {SEPARATOR}")
 
 
-# 元となる DataFrame `df1_1`, `df2_1` を作る。
-df1_1 = pd.DataFrame({"col1": [i for i in range(5)]})
-print_heading("df1_1")
-print(df1_1.to_markdown())
+# 元となる DataFrame `df1`, `df2` を作る。
+df1 = pd.DataFrame({"col1": [i for i in range(5)]})
+print_heading("df1")
+print(df1.to_markdown())
 # output
 """
- ===== ===== =====  df1_1  ===== ===== =====
+ ===== ===== =====  df1  ===== ===== =====
 |    |   col1 |
 |---:|-------:|
 |  0 |      0 |
@@ -25,12 +25,12 @@ print(df1_1.to_markdown())
 |  3 |      3 |
 |  4 |      4 |
 """
-df2_1 = pd.DataFrame({"col2": [i for i in range(5, 10)]})
-print_heading("df2_1")
-print(df2_1.to_markdown())
+df2 = pd.DataFrame({"col2": [i for i in range(5, 10)]})
+print_heading("df2")
+print(df2.to_markdown())
 # output
 """
- ===== ===== =====  df2_1  ===== ===== =====
+ ===== ===== =====  df2  ===== ===== =====
 |    |   col2 |
 |---:|-------:|
 |  0 |      5 |
@@ -40,37 +40,37 @@ print(df2_1.to_markdown())
 |  4 |      9 |
 """
 
-# `df1_1`, `df2_1` を一部切り出す。
-df1_2 = df1_1.iloc[:2]
-print_heading("df1_2")
-print(df1_2.to_markdown())
+# `df1`, `df2` を一部切り出す。
+df1_sliced = df1.iloc[:2]
+print_heading("df1_sliced")
+print(df1_sliced.to_markdown())
 # output
 """
- ===== ===== =====  df1_2  ===== ===== =====
+ ===== ===== =====  df1_sliced  ===== ===== =====
 |    |   col1 |
 |---:|-------:|
 |  0 |      0 |
 |  1 |      1 |
 """
 
-df2_2_ng = df2_1.loc[3:4]
-print_heading("df2_2_ng")
-print(df2_2_ng.to_markdown())
+df2_sliced_simply = df2.loc[3:4]
+print_heading("df2_sliced_simply")
+print(df2_sliced_simply.to_markdown())
 # output
 """
- ===== ===== =====  df2_2_ng  ===== ===== =====
+ ===== ===== =====  df2_sliced_simply  ===== ===== =====
 |    |   col2 |
 |---:|-------:|
 |  3 |      8 |
 |  4 |      9 |
 """
 
-df3_1 = pd.concat([df1_2, df2_2_ng], axis="columns")
-print_heading("df3_1")
-print(df3_1.to_markdown())
+df3_ng = pd.concat([df1_sliced, df2_sliced_simply], axis="columns")
+print_heading("df3_ng")
+print(df3_ng.to_markdown())
 # output
 """
- ===== ===== =====  df3_1  ===== ===== =====
+ ===== ===== =====  df3_ng  ===== ===== =====
 |    |   col1 |   col2 |
 |---:|-------:|-------:|
 |  0 |      0 |    nan |
@@ -80,12 +80,12 @@ print(df3_1.to_markdown())
 """
 
 # この状態で複数列を使った演算を行っても下記のようになる。
-df3_1["col1_plus_col2"] = df3_1["col1"] + df3_1["col2"]
-print_heading("df3_1")
-print(df3_1.to_markdown())
+df3_ng["col1_plus_col2"] = df3_ng["col1"] + df3_ng["col2"]
+print_heading("df3_ng")
+print(df3_ng.to_markdown())
 # output
 """
- ===== ===== =====  df3_1  ===== ===== =====
+ ===== ===== =====  df3_ng  ===== ===== =====
 |    |   col1 |   col2 |   col1_plus_col2 |
 |---:|-------:|-------:|-----------------:|
 |  0 |      0 |    nan |              nan |
@@ -95,36 +95,36 @@ print(df3_1.to_markdown())
 """
 
 # 意図した DataFrame を作るためには `reset_index()` を行う必要がある。
-df2_2_ok = df2_1.loc[3:4].reset_index(drop=True)
-print_heading("df2_2_ok")
-print(df2_2_ok.to_markdown())
+df2_sliced_reset_index = df2.loc[3:4].reset_index(drop=True)
+print_heading("df2_sliced_reset_index")
+print(df2_sliced_reset_index.to_markdown())
 # output
 """
- ===== ===== =====  df2_2_ok  ===== ===== =====
+ ===== ===== =====  df2_sliced_reset_index  ===== ===== =====
 |    |   col2 |
 |---:|-------:|
 |  0 |      8 |
 |  1 |      9 |
 """
 
-df4_1 = pd.concat([df1_2, df2_2_ok], axis="columns")
-print_heading("df4_1")
-print(df4_1.to_markdown())
+df3_ok = pd.concat([df1_sliced, df2_sliced_reset_index], axis="columns")
+print_heading("df3_ok")
+print(df3_ok.to_markdown())
 # output
 """
- ===== ===== =====  df4_1  ===== ===== =====
+ ===== ===== =====  df3_ok  ===== ===== =====
 |    |   col1 |   col2 |
 |---:|-------:|-------:|
 |  0 |      0 |      8 |
 |  1 |      1 |      9 |
 """
 
-df4_1["col1_plus_col2"] = df4_1["col1"] + df4_1["col2"]
-print_heading("df4_1")
-print(df4_1.to_markdown())
+df3_ok["col1_plus_col2"] = df3_ok["col1"] + df3_ok["col2"]
+print_heading("df3_ok")
+print(df3_ok.to_markdown())
 # output
 """
- ===== ===== =====  df4_1  ===== ===== =====
+ ===== ===== =====  df3_ok  ===== ===== =====
 |    |   col1 |   col2 |   col1_plus_col2 |
 |---:|-------:|-------:|-----------------:|
 |  0 |      0 |      8 |                8 |
